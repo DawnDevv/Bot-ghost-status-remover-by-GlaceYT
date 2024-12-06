c09a5031e468a7153dc8c743b611934b7353cb2c
DawnDevv <mrsiet8901@gmail.com>
2024-12-05 20:34:36 -0500

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /*
-
 ☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
                                                  
   _________ ___ ___ ._______   _________    
@@ -13,9 +12,8 @@ DISCORD :  https://discord.com/invite/xQF9f9yUEM
 YouTube : https://www.youtube.com/@GlaceYT                         
                                                                        
 ☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
-
-
 */
+
 const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 require('dotenv').config();
 const express = require('express');
@@ -37,11 +35,6 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
-let currentStatusIndex = 0;
-let currentTypeIndex = 0;
-
 async function login() {
   try {
     await client.login(process.env.TOKEN);
@@ -54,16 +47,14 @@ async function login() {
   }
 }
 
+// Update status to "Playing on the ISA"
 function updateStatus() {
-  const currentStatus = statusMessages[currentStatusIndex];
-  const currentType = statusTypes[currentTypeIndex];
+  const customStatus = "Playing on the ISA"; // Custom status message
   client.user.setPresence({
-    activities: [{ name: currentStatus, type: ActivityType.Custom }],
-    status: currentType,
+    activities: [{ name: customStatus, type: ActivityType.Playing }], // Set activity type to Playing
+    status: 'online', // Set status to online (can also be 'dnd', 'idle', or 'invisible')
   });
-  console.log('\x1b[33m[ STATUS ]\x1b[0m', `Updated status to: ${currentStatus} (${currentType})`);
-  currentStatusIndex = (currentStatusIndex + 1) % statusMessages.length;
-  currentTypeIndex = (currentTypeIndex + 1) % statusTypes.length;
+  console.log('\x1b[33m[ STATUS ]\x1b[0m', `Updated status to: ${customStatus}`);
 }
 
 function heartbeat() {
@@ -74,16 +65,13 @@ function heartbeat() {
 
 client.once('ready', () => {
   console.log('\x1b[36m[ INFO ]\x1b[0m', `\x1b[34mPing: ${client.ws.ping} ms \x1b[0m`);
-  updateStatus();
-  setInterval(updateStatus, 10000);
-  heartbeat();
+  updateStatus(); // Set the status when the bot is ready
+  heartbeat(); // Log the heartbeat every 30 seconds
 });
 
 login();
 
-  
 /*
-
 ☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
                                                  
   _________ ___ ___ ._______   _________    
@@ -97,6 +85,4 @@ DISCORD :  https://discord.com/invite/xQF9f9yUEM
 YouTube : https://www.youtube.com/@GlaceYT                         
                                                                        
 ☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
-
-
 */
